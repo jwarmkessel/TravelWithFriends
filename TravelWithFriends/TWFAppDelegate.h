44f7bdf316ec5b92488b170aecf11d7af2dcad97
jwarmkessel <justin@rewardme.com>
2013-03-02 13:10:56 -0800

    Initial Commit

---
+++ TravelWithFriends/TWFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWFAppDelegate.h
+//  TravelWithFriends
+//
+//  Created by Justin Warmkessel on 3/2/13.
+//  Copyright (c) 2013 Justin Warmkessel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravelWithFriends/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TravelWithFriends
+//
+//  Created by Justin Warmkessel on 3/2/13.
+//  Copyright (c) 2013 Justin Warmkessel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWFAppDelegate class]));
+    }
+}
